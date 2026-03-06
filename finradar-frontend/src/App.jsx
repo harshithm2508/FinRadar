6d40b1e69a343e2f3f4236657b970fdc4752cd4f
Harshith Muthangi <harshithm2508@gmail.com>
2026-03-06 16:48:58 +0530

    frontend with year, month, week & day expenses

--- finradar-frontend/src/App.jsx
+++ finradar-frontend/src/App.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return(
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello FinRadar
-      </h1>
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchExpenses() {
+      try {
+        const res = await axios.get("https://finradar-6nxy.onrender.com/expense/present");
+        setData(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchExpenses();
+  }, []);
+
+  if (!data) {
+    return <div className="text-black p-10">Loading...</div>;
+  }
+
+  return (
+    <div className="text-black p-10">
+      <h2>Today's Expense: ₹{data.dayExpense}</h2>
+      <h2>This Week: ₹{data.weekExpense}</h2>
+      <h2>This Month: ₹{data.monthExpense}</h2>
+      <h2>This Year: ₹{data.yearExpense}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+//  https://finradar-6nxy.onrender.com/expense/present
