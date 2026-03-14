5e5ab979c7e0637a1656cd96c1df0d0970e49ced
Harshith Muthangi <harshithm2508@gmail.com>
2026-03-14 23:41:10 +0530

    browserrouter to main.jsx

--- finradar-frontend/src/App.jsx
+++ finradar-frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Today from "./Today";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/today" element={<Today />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/today" element={<Today />} />
+    </Routes>
   );
 }
 
