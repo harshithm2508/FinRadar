bcb475fdcdd47dec59ac0cf8508e42af27d7cd0b
Harshith Muthangi <harshithm2508@gmail.com>
2026-03-14 23:47:39 +0530

    frontend to fetch today's transactions

--- finradar-frontend/src/Home.jsx
+++ finradar-frontend/src/Home.jsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-function App() {
-
+function Home() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
         async function fetchExpenses() {
         try {
-            const res = await axios.get("https://finradar-6nxy.onrender.com/expense/present");
+            const res = await axios.get(
+            "https://finradar-6nxy.onrender.com/expense/present"
+            );
             setData(res.data);
         } catch (err) {
             console.error(err);
         }
-    }
+        }
 
         fetchExpenses();
     }, []);
@@ -28,10 +30,17 @@ function App() {
         <h2>This Week: ₹{data.weekExpense}</h2>
         <h2>This Month: ₹{data.monthExpense}</h2>
         <h2>This Year: ₹{data.yearExpense}</h2>
+
+        <div className="mt-6">
+            <Link
+            to="/today"
+            className="text-blue-600 underline"
+            >
+            View Today's Transactions
+            </Link>
+        </div>
         </div>
     );
 }
 
-export default App;
-
-// https://finradar-6nxy.onrender.com
+export default Home;
